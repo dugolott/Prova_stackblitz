59cdf44053742eed5a1b3b3cbadf34f90a62c03a
dugolott <ramon.ugolotti@itis.pr.it>
2021-12-29 01:47:03 +0100

    3 files modified

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log('hello!')
+let n=0;
+let visitor = "";
+
+window.onload =function(e){
+  document.querySelector("input").addEventListener("click", count);
+  visitor = document.querySelector("h4").innerText;
+  count();
+};
+
+function count(){
+  n++;
+  console.log("n = "+n);
+  document.querySelector("h4").innerHTML = visitor + "<span>"+n+"</span>"
+}
